fb333aceea183e980dfb36a5851f1c208db7c2ed
Roman K <jumji19992412@gmail.com>
2026-07-08 21:42:37 +0300

    config: add vuetify package

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+<div>
+    <h1>Hello World</h1>
+    <v-btn>
+        Button
+    </v-btn>
+</div>
 </template>
+<script setup lang="ts">
+
+</script>
